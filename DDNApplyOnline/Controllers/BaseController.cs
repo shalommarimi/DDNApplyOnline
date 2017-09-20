8f877bf6357c94723070a7bc15a519abac78a371
shalommarimi <shalommarimi@gmail.com>
2017-09-20 19:07:10 +0200

    Modified Services

---
+++ DDNApplyOnline/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using ApplyOnline.DataContext;
+using System.Web.Http;
+
+namespace DDNApplyOnline.Controllers
+{
+    public class BaseController : ApiController
+    {
+        ApplicantsDbContext DbContext = new ApplicantsDbContext();
+    }
+}
